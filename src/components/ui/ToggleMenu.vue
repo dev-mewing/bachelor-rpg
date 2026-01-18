da74dde0c64886f959ebe960caa0b6bc42c1c7e8
devmewing <devmewing@gmail.com>
2026-01-18 18:05:27 -0500

    Show points in ToggleMenu, styles updated

--- src/components/ui/ToggleMenu.vue
+++ src/components/ui/ToggleMenu.vue
@@ -1,6 +1,13 @@
 <script setup>
   import Menu from '@/components/ui/Menu.vue';
+  import { usePlayerStore } from '@/stores/player';
   import { ref } from 'vue';
+
+  const playerStore = usePlayerStore();
+  
+  const loadPlayerData = () => {
+    playerStore.loadFromStorage();
+  }
 
   const isOpen = ref(false);
 
@@ -12,8 +19,11 @@
 </script>
 <template>
   <div class="container menu-container">
-    <img src="@/assets/images/brand/bachelor-rpg-logo-trans.png" alt="Bachelor RPG Logo" width="45" />
-    <button type="button" @click="isOpen = !isOpen">
+    <div class="small-player-info">
+      Lv. {{ playerStore.level }}
+    </div>
+    <img class="main-logo" src="@/assets/images/brand/bachelor-rpg-logo-trans.png" alt="Bachelor RPG Logo" width="45" />
+    <button class="open-btn" type="button" @click="isOpen = !isOpen">
       <span class="fa fa-bars" title="Open menu"></span>
     </button>
     <Menu :isOpen="isOpen" @close="closeMenu" />
@@ -21,8 +31,29 @@
 </template>
 
 <style scoped>
+  .main-logo {
+    width: 66px;
+    justify-self: center
+  }
   .menu-container {
-    display: flex;
-    justify-content: space-between;
+    display: grid;
+    /* justify-items: center; */
+    align-items: center;
+    grid-template-columns: 1fr 2fr 1fr;
+  }
+  .open-btn {
+    justify-self: right;
+    font-size: 1.8rem;
+    background-color: transparent;
+    color: maroon;
+    border: none;
+    margin: 0;
+    padding: 0;
+    transform: unset;
+  }
+  .small-player-info {
+    color: maroon;
+    font-weight: bold;
+    font-size: 0.8rem;
   }
 </style>
